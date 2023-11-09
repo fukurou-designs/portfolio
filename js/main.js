439ba40d1537a47684e2cd07bf625f7cb6da6954
fukurou-designs <150230733+fukurou-designs@users.noreply.github.com>
2023-11-09 11:12:59 +0900

    header変更

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", function(){
 
-    //プラグインを定義
+
     gsap.registerPlugin(ScrollTrigger);
     
     const area  = document.querySelector(".js-area");
@@ -8,35 +8,35 @@ window.addEventListener("load", function(){
     const items = document.querySelectorAll(".js-item");
     const num   = items.length;
     
-    //横幅を指定
+
     gsap.set(wrap,  { width: num * 100 + "%" });
     gsap.set(items, { width: 100 / num + "%" });
     
     gsap.to(items, {
-      xPercent: -100 * ( num - 1 ), //x方向に移動させる
+      xPercent: -100 * ( num - 1 ), 
       ease: "none",
       scrollTrigger: {
-        trigger: area, //トリガー
-        start: "top top", //開始位置
-        end: "+=4000", //終了位置 スクロール量の調整
-        pin: true, //ピン留め
-        scrub: true, //スクロール量に応じて動かす
+        trigger: area, 
+        start: "top top", 
+        end: "+=4000", 
+        pin: true, 
+        scrub: true, 
       }
     });
 });
 
 window.addEventListener("load", function(){
 
-  //プラグインを定義
+
   gsap.registerPlugin(ScrollTrigger);
   
   const text = document.querySelectorAll(".js-text");
 
   gsap.to(text, {
-  delay: 1, //アニメーションの初期遅延（アニメーションが開始されるまでの指定）
-  duration: 1, //アニメーションの時間の設定
-  opacity: 1, //表示状態の指定
-  y: 0, //cssでY軸-20pxから完了時0pxへの指定
+  delay: 1,
+  duration: 1, 
+  opacity: 1, 
+  y: 0, 
 
   scrollTrigger: {
   trigger: text,
